3a28e2657ee919a4a4828363736ee56432e8495e
Alex Calia <alex.m.calia@outlook.com>
2020-08-24 14:59:07 -0400

    styling complete, large and small display menus working, media queries done

--- scripts/index.js
+++ scripts/index.js
@@ -1,48 +1,110 @@
-const portfolio = {};
+const index = {};
 
-portfolio.menu = document.getElementById("sideMenu");
-portfolio.navItemName = document.getElementsByClassName("navItemName");
+index.$menu = $('#sideMenu');
+index.$navItemName = $('.navItemName')
+index.$home = $('#home');
+index.$about=$('#about');
+index.skills = $('#skills');
+index.works = $('#works');
+index.$contact = $('#contact');
+index.$menuButton = $('#menuButton');
+index.$isLargeWindow = true;
 
-/*********************/
-/*   MENU FUNCTIONS  */
-/*********************/
-portfolio.showMenu = () => {
-  portfolio.menu.style.width = "220px";
+index.showHideMenu = function () {
+  index.$menu.toggleClass('sideMenuHide sideMenuShow');
 }
 
-portfolio.hideMenu = () => {
-  portfolio.menu.style.width = "0px";
+if ($(window).width() <= 990) {
+  index.$menu.addClass('sideMenuHide').removeClass('sideMenuShow');
 }
 
-portfolio.hoverMenuOn = () => {
-  portfolio.menu.style.width = "220px";
-}
+index.$menuButton.on('click', index.showHideMenu);
 
-portfolio.hoverMenuOff = () => {
-  portfolio.menu.style.width = "105px";
-}
-
-portfolio.menuHoverListener = function(){
-  portfolio.menu.addEventListener("mouseenter", ()=> {
-    portfolio.hoverMenuOn();
-    setTimeout(()=>{
-      for (let i = 0; i < portfolio.navItemName.length; i++) {
-        console.log(portfolio.navItemName[i]);
-        portfolio.navItemName[i].classList.remove("visuallyHidden");
-      }
-    }, 250)
-  })
-
-  portfolio.menu.addEventListener("mouseleave", ()=>{
-    portfolio.hoverMenuOff();
-    for (let i = 0; i < portfolio.navItemName.length; i++) {
-      portfolio.navItemName[i].classList.add("visuallyHidden");
+// Document Ready
+$(function () {
+  $(window).on('resize', function () {
+    if ($(window).width() > 990) {
+      index.$menu.removeClass('sideMenuHide').addClass('sideMenuShow')
+    } else {
+      index.$menu.removeClass('sideMenuShow').addClass('sideMenuHide');
     }
   })
-}
-
-document.addEventListener('DOMContentLoaded', (event) => {
-  portfolio.menuHoverListener();
 })
 
+
+
+
+
+// index.mobileMenu = function () {
+//   index.$menu.addClass('sideMenuHide').removeClass('sideMenuShow').addClass('hoverMenu');
+//   index.$navItemName.removeClass("navItemHide").addClass("navItemShow");
+// }
+
+// index.largeDisplay = function () {
+//   index.$menu.addClass('sideMenuShow').removeClass('sideMenuHide');
+//   index.$navItemName.removeClass("navItemShow").addClass("navItemHide");
+//   index.$menu.removeClass("hoverMenu");
+//   index.menuHover();
+// }
+
+// index.menuFunction = function () {
+//   if ($(window).width() > 990) {
+//     index.largeDisplay();
+//   } else if ($(window).width() <= 990) {
+//     index.mobileMenu();
+//     index.showHideMenu();
+//   }
+// }
+
+// index.menuHover = function() {
+//     index.$menu.mouseenter(function(){
+//       console.log('hey');
+//       $(this).toggleClass("hoverMenu");
+//       index.$navItemName.toggleClass("navItemHide navItemShow")
+//     }).mouseleave(function(){
+//       $(this).toggleClass("hoverMenu");
+//       index.$navItemName.toggleClass("navItemHide navItemShow")
+//     });
+// }
+
+// // Event listeners
+// index.eventListeners = function() {
+//   index.$menuButton.on('click', index.showHideMenu);
+//   $(window).resize(function(){
+//     if ($(window).width() <= 990) {
+//       index.mobileMenu();
+//     } else {
+//       index.largeDisplay();
+//     }
+//   })
+// }
+
+// // init method
+// index.init = function() {
+//   // index.eventListeners();
+// }
+
+// index.menuControl = function () {
+  
+//   if ($(window).width() > 990) {
+//     index.$menu.mouseenter(function(){
+//       console.log('hey');
+//     }).mouseleave(function(){
+//     })
+//   } else {
+//     console.log('hello');
+//   }
+// }
+
+// index.init = function () {
+//   index.menuControl();
+// }
+
+// index.$navItemName.hide();
+
+// // Document Ready
+// $(function () {
+//   index.init();
+//   $(window).on('resize', index.init);
+// })
 
