6f7b8a79bfbca2ae978dd2be1c3c72493339d55f
Alex Calia <alex.m.calia@outlook.com>
2020-08-24 18:36:50 -0400

    accessibility sylings, smooth scrolling added, deployed

--- scripts/index.js
+++ scripts/index.js
@@ -1,110 +1,77 @@
 const index = {};
 
 index.$menu = $('#sideMenu');
+index.$navItem = $('.navItem');
 index.$navItemName = $('.navItemName')
 index.$home = $('#home');
 index.$about=$('#about');
-index.skills = $('#skills');
-index.works = $('#works');
+index.$skills = $('#skills');
+index.$works = $('#works');
 index.$contact = $('#contact');
 index.$menuButton = $('#menuButton');
-index.$isLargeWindow = true;
+index.$scrollDown = $('#scrollDown');
+index.isOpen = false;
 
-index.showHideMenu = function () {
-  index.$menu.toggleClass('sideMenuHide sideMenuShow');
+// Scroll function
+index.scroll = function(target) {
+  $('html,body').animate({ scrollTop: $(target).offset().top }, 500);
 }
 
+// Menu button function
+index.showHideMenu = function() {
+  index.$menu.toggleClass('sideMenuHide sideMenuShow');
+  index.isOpen = !index.isOpen;
+}
+
+// Mobile class changes on initial load
 if ($(window).width() <= 990) {
   index.$menu.addClass('sideMenuHide').removeClass('sideMenuShow');
+  index.$scrollDown.hide();
 }
 
-index.$menuButton.on('click', index.showHideMenu);
-
-// Document Ready
-$(function () {
-  $(window).on('resize', function () {
+index.eventListeners = function() {
+  // when windox resizes between large and small displayes
+  $(window).on('resize', function() {
     if ($(window).width() > 990) {
       index.$menu.removeClass('sideMenuHide').addClass('sideMenuShow')
+      index.$scrollDown.show();
+      index.isOpen= false;
     } else {
       index.$menu.removeClass('sideMenuShow').addClass('sideMenuHide');
+      index.isOpen = false;
+      index.$scrollDown.hide();
+    }
+  });
+
+  // Nav menu click events for scroll function
+  $('a[href*=\\#]').on('click', function () {
+    index.scroll(this.hash);
+  });
+
+  // Menu button click event
+  index.$menuButton.on('click', index.showHideMenu);
+
+  // Hide menu when clicking a link
+  index.$navItem.on('click', function() {
+    if (index.isOpen) {// Hide menu when clicking a link
+      index.showHideMenu();
+    }
+  });
+
+  // Menu button ENTER key event
+  index.$menuButton.on('keypress', function(e){
+    if (e.which === 13) {
+      $(this).trigger('click');
     }
   })
+}
+
+// init method
+index.init = function() {
+  index.eventListeners();
+}
+
+// Document Ready
+$(function() {
+  index.init();
 })
-
-
-
-
-
-// index.mobileMenu = function () {
-//   index.$menu.addClass('sideMenuHide').removeClass('sideMenuShow').addClass('hoverMenu');
-//   index.$navItemName.removeClass("navItemHide").addClass("navItemShow");
-// }
-
-// index.largeDisplay = function () {
-//   index.$menu.addClass('sideMenuShow').removeClass('sideMenuHide');
-//   index.$navItemName.removeClass("navItemShow").addClass("navItemHide");
-//   index.$menu.removeClass("hoverMenu");
-//   index.menuHover();
-// }
-
-// index.menuFunction = function () {
-//   if ($(window).width() > 990) {
-//     index.largeDisplay();
-//   } else if ($(window).width() <= 990) {
-//     index.mobileMenu();
-//     index.showHideMenu();
-//   }
-// }
-
-// index.menuHover = function() {
-//     index.$menu.mouseenter(function(){
-//       console.log('hey');
-//       $(this).toggleClass("hoverMenu");
-//       index.$navItemName.toggleClass("navItemHide navItemShow")
-//     }).mouseleave(function(){
-//       $(this).toggleClass("hoverMenu");
-//       index.$navItemName.toggleClass("navItemHide navItemShow")
-//     });
-// }
-
-// // Event listeners
-// index.eventListeners = function() {
-//   index.$menuButton.on('click', index.showHideMenu);
-//   $(window).resize(function(){
-//     if ($(window).width() <= 990) {
-//       index.mobileMenu();
-//     } else {
-//       index.largeDisplay();
-//     }
-//   })
-// }
-
-// // init method
-// index.init = function() {
-//   // index.eventListeners();
-// }
-
-// index.menuControl = function () {
-  
-//   if ($(window).width() > 990) {
-//     index.$menu.mouseenter(function(){
-//       console.log('hey');
-//     }).mouseleave(function(){
-//     })
-//   } else {
-//     console.log('hello');
-//   }
-// }
-
-// index.init = function () {
-//   index.menuControl();
-// }
-
-// index.$navItemName.hide();
-
-// // Document Ready
-// $(function () {
-//   index.init();
-//   $(window).on('resize', index.init);
-// })
-
